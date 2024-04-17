42aec4ea3f74959d37f5be664a8d7efc49a09943
Aleksander <111000626+Michnowicz@users.noreply.github.com>
2024-04-17 21:32:28 +0200

    Delete fonctions directory

--- fonctions/place.py
+++
@@ -1,9 +0,0 @@
-
-
-class Place:
-    def __init__(self,name, objects, ennemies, bosses):
-        self.name = name
-        self.objects = objects
-        self.ennemies = ennemies
-        self.bosses = bosses
-
